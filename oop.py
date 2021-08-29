80634088950360a11ed63bba7afb08b2fab28bb7
bloo0 <phil@macbook-air>
2021-08-29 21:14:34 +0800

    add static method

--- oop.py
+++ oop.py
@@ -104,5 +104,24 @@ print(Person.number_of_people_())
 
 
 
+## 
+
+class Math:
+
+	@staticmethod
+	def add5(x):
+		return x + 5
+
+	@staticmethod
+	def pr():
+		print('run')
+
+print(Math.add5(5))
+Math.pr()
+
+
+
+
+
 
 
